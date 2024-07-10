813342d919038bac90344c9ccc15d6f9d5b4e1a3
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2024-07-10 06:56:00 +0100

    Refactor: build file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,15 +5,16 @@ import morgan from "morgan";
 import { router } from "./app/routes";
 
 const app = express();
+const pathToStatic = path.join(__dirname, "../../frontend/dist");
 
 app.use(morgan("dev"));
-app.use(express.static(path.join(process.cwd(), "../../frontend/dist")));
+app.use(express.static(pathToStatic));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(process.cwd(), "../../frontend/dist/index.html"));
+  res.sendFile(path.join(pathToStatic, "index.html"));
 });
 
 export { app };
